75f909a2fa86042233624e5c4cdd2c50a0f7c56e
Alexey Lunacharsky <alun@katlex.com>
2016-04-22 00:08:21 +0100

    Updated files on CDN

--- index.js
+++ index.js
@@ -69,7 +69,14 @@
 	ready.then(function () {
 	  document.body.appendChild(elem);
 	  document.body.style.margin = 0;
+
 	  render();
+
+	  var ribbon = document.createElement('div');
+	  ribbon.innerHTML = __webpack_require__(3);
+	  ribbon.style.right = 0;
+	  ribbon.style.top = 0;
+	  document.body.appendChild(ribbon);
 	});
 
 	function render() {
@@ -313,6 +320,12 @@
 	})(Circle);
 
 	module.exports = Circle;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = "<a href=\"https://github.com/alun/ireland\"><img style=\"position: absolute; top: 0; right: 0; border: 0;\" src=\"https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67\" alt=\"Fork me on GitHub\" data-canonical-src=\"https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png\"></a>\n"
 
 /***/ }
 /******/ ]);
